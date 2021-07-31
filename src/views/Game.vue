073570422838f7260b343ea1d137cad0daa17bca
Ana Tomboulian <ana@peerlogic.com>
2021-07-31 15:23:41 -0700

    layout of page established

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,5 +1,55 @@
 <template>
-  <div class="about">
-    <h1>Game on!</h1>
+  <div>
+    <v-container fluid class="grey lighten-5 d-flex align-content-lg-space-between flex-column" style="min-height: 100vh" >
+      <v-row align-stretch>
+        <v-col>
+          <v-card
+            height="100%"
+            class="pa-2"
+            outlined
+            tile
+          >
+            <v-icon>mdi-virus</v-icon>
+            Virus
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card
+            height="100%"
+            class="pa-2"
+            outlined
+            tile
+          >
+            <v-icon>mdi-newspaper-variant-outline</v-icon>
+            Current Card Area
+          </v-card>
+        </v-col>
+        <v-col
+        >
+          <v-card
+            class="pa-2"
+            outlined
+            tile
+            height="100%"
+          >
+           <v-icon>mdi-newspaper-variant-multiple-outline</v-icon>
+            Event Card Deck
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row align-stretch>
+        <v-col>
+          <v-card
+            height="100%"
+            class="pa-2"
+            outlined
+            tile
+          >
+            <v-icon>mdi-city-variant</v-icon>
+            Cities
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
